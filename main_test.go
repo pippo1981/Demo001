b0c2f33ecf6a7601d7a9d2fe4ae381fcf71f23a0
Allen SU <pippo_1981@hotmail.com>
2020-05-02 09:34:39 -0700

    .

--- main_test.go
+++ main_test.go
@@ -1,21 +1,15 @@
 package main
 
 import (
-   "log"
-   "net/http"
+    "fmt"
+    "net/http"
 )
 
-type Server struct{}
-
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-   w.WriteHeader(http.StatusOK)
-   w.Header().Set("Content-Type", "application/json")
-   w.Write([]byte(`{"message": "hello world"}`))
-}
-
 func main() {
-   s := &Server{}
-   http.Handle("/", s)
-   log.Fatal(http.ListenAndServe(":8080", nil))
+    http.HandleFunc("/", HelloServer)
+    http.ListenAndServe(":8080", nil)
 }
 
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
